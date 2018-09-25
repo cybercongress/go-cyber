2985ffd8ac2760a7e00438d6be7268d5bf461fdd
Artur Albov <artur.albov@gmail.com>
2018-09-25 18:56:18 +0300

    Fix docker image

--- cosmos/poc/entrypoint.sh
+++ cosmos/poc/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-cp -n /genesis.json /root/.cyberd/config/
-cp -n /config.toml /root/.cyberd/config/
+if [ ! -f "/root/.cyberd/config/genesis.json" ]
+then
+    cp /genesis.json /root/.cyberd/config/
+fi
+
+if [ ! -f "/root/.cyberd/config/config.toml" ]
+then
+    cp /config.toml /root/.cyberd/config/
+fi
 
 cat /root/.cyberd/config/genesis.json
 
