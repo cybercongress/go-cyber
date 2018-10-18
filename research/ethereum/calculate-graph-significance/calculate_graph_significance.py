851f573554e3b4b91abe3c7fec7f1f818679420e
Hleb Albau <hleb.albau@gmail.com>
2018-10-18 13:01:20 +0300

    #21 Fix Python path

--- research/ethereum/calculate-graph-significance/calculate_graph_significance.py
+++ research/ethereum/calculate-graph-significance/calculate_graph_significance.py
@@ -1,9 +1,8 @@
 import os
 import sys
 
-from common.calculate_significance import test_ranks_significance
-
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
+from common.calculate_significance import test_ranks_significance
 
 import glob
 from collections import OrderedDict
