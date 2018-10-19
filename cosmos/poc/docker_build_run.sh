17f08daa173220dbba91b83bc1e7868b45c8a9ab
Artur Albov <artur.albov@gmail.com>
2018-10-19 15:54:38 +0300

    Add cyberdproxy process to docker container. Add status endpoint

--- cosmos/poc/docker_build_run.sh
+++ cosmos/poc/docker_build_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t cybercongress/cyberd .
-docker run -d --restart always --name=cyberd -p 34656:26656 -p 34657:26657 -v /cyberdata/cyberd/data:/root/.cyberd/data -v /cyberdata/cyberd/config:/root/.cyberd/config cybernode/cyberd:master
+docker run -d --restart always --name=cyberd -p 34656:26656 -p 34657:26657 -p 34660:26660 -v /cyberdata/cyberd/data:/root/.cyberd/data -v /cyberdata/cyberd/config:/root/.cyberd/config cybernode/cyberd:master
