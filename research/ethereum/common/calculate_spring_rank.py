#21 Fix violation calculation performance

research/ethereum/common/calculate_spring_rank.py
@@ -66,7 +66,7 @@ def calculate_spring_rank(A, initial_x=None):
         iterations += 1
 
     print_with_time("Solving Bx=b equation using 'bicgstab' iterative method")
-    result = scipy.sparse.linalg.bicgstab(B, b, x0=initial_x, callback=bicgstab_callback, tol=1e-2, atol=0)
+    result = scipy.sparse.linalg.bicgstab(B, b, x0=initial_x, callback=bicgstab_callback, tol=1e-3)
 
     if result[1] != 0:
         print_with_time("Can't solve Bx=b")
@@ -105,7 +105,7 @@ def update_rank(graph: DiGraph, rank: Rank, new_edges: List[Edge]) -> (int, Rank
     return iterations, rank
 
 
-def calculate_violations(A: csr_matrix, rank: [float]) -> (float, float):
+def calculate_violations(A: csr_matrix, rank: [float]) -> (float, float, float):
     """
     Calculate number of violations in a graph given SpringRank scores
     A violaton is an edge going from a lower ranked node to a higher ranked one
@@ -123,11 +123,11 @@ def calculate_violations(A: csr_matrix, rank: [float]) -> (float, float):
     # All elements below main triangle is connection from low-ranked node
     # to high-ranked node. So to get all violation just sum all elements
     # below main triangle.
-    viol = scipy.sparse.tril(A_sort, -1).sum()
+    viol = scipy.sparse.tril(A_sort).sum()
 
     m = A_sort.sum()  # All matrix weights sum
 
-    return (viol, viol / m)
+    return (viol, viol / m, m)
 
 
 def calculate_min_violations(A: csr_matrix) -> (float, float):
@@ -142,17 +142,19 @@ def calculate_min_violations(A: csr_matrix) -> (float, float):
         proportion of all edges against minimum violations
     """
 
-    min_viol = 0
-    for i in range(A.shape[0]):
-        for j in range(i + 1, A.shape[0] - 1):
-            if A[i, j] > 0 and A[j, i] > 0:
-                min_viol = min_viol + min(A[i, j], A[j, i])
+    ii, ji, v = scipy.sparse.find(A)  # I,J,V contain the row, column indices, and values of the nonzero entries.
+
+    min_viol = 0.0
+    for e in range(len(v)):  # for all nodes interactions
+        i, j = ii[e], ji[e]
+        if A[i, j] > 0 and A[j, i] > 0:
+            min_viol = min_viol + min(A[i, j], A[j, i])
 
     m = A.sum()
     return (min_viol, min_viol / m)
 
 
-def calculate_system_violated_energy(A: csr_matrix, rank: [float]) -> (float, float):
+def calculate_system_violated_energy(A: csr_matrix, rank: [float]) -> (float, float, float):
     """
     Calculate number of violations in a graph given SpringRank scores
     A violaton is an edge going from a lower ranked node to a higher ranked one
@@ -166,13 +168,13 @@ def calculate_system_violated_energy(A: csr_matrix, rank: [float]) -> (float, fl
     """
     i, j, v = scipy.sparse.find(A)  # I,J,V contain the row indices, column indices, and values of the nonzero entries.
     normed_rank = (rank - min(rank)) / (max(rank) - min(rank))  # normalize
-    wv = 0
+    wv = 0.0
     for e in range(len(v)):  # for all nodes interactions
         if normed_rank[i[e]] < normed_rank[j[e]]:  # compare ranks of two nodes i and j
             wv = wv + v[e] * (normed_rank[j[e]] - normed_rank[i[e]])
 
     H = calculate_Hamiltonion(A, rank)
-    return (wv, wv / H)
+    return (wv, wv / H, H)
 
 
 def calculate_Hamiltonion(A: csr_matrix, rank: [float]) -> float:
@@ -186,9 +188,11 @@ def calculate_Hamiltonion(A: csr_matrix, rank: [float]) -> float:
     """
 
     H = 0.0
+    normed_rank = (rank - min(rank)) / (max(rank) - min(rank))  # normalize
 
-    for i in range(A.shape[0]):
-        for j in range(A.shape[1]):
-            H = H + 0.5 * A[i, j] * (rank[i] - rank[j] - 1) ** 2
+    ii, ji, v = scipy.sparse.find(A)  # I,J,V contain the row, column indices, and values of the nonzero entries.
+    for e in range(len(v)):  # for all nodes interactions
+        i, j = ii[e], ji[e]
+        H = H + 0.5 * A[i, j] * (normed_rank[i] - normed_rank[j] - 1) ** 2
 
     return H