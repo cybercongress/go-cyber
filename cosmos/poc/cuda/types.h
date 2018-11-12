3f42f37dbe086bb66f02c08a147835c616a19541
Hleb Albau <hleb.albau@gmail.com>
2018-11-12 19:19:34 +0300

    #74 Calculate compressed in links count for each cid

--- cosmos/poc/cuda/types.h
+++ cosmos/poc/cuda/types.h
@@ -3,7 +3,9 @@
 
 #include <stdint.h>
 
-/* Go -> CGO passing types */
+/*******************************************/
+/* REPRESENTS LINK                         */
+/*******************************************/
 typedef struct {
     /* Index of opposite cid in cids array */
     uint64_t opposite_cid_index;
@@ -11,6 +13,9 @@ typedef struct {
     uint64_t user_index;
 } cid_link;
 
+/*******************************************/
+/* REPRESENTS CID                          */
+/*******************************************/
 typedef struct {
 	uint64_t in_links_start_index;
 	uint64_t in_links_count;
@@ -18,5 +23,18 @@ typedef struct {
 	uint64_t out_links_start_index;
 	uint64_t out_links_count;
 } cid;
+
+/*******************************************/
+/* REPRESENTS INCOMING LINK WITH IT WEIGHT */
+/*******************************************/
+/* Finds maximum rank difference for single element  */
+/*                                                   */
+/*****************************************************/
+typedef struct {
+    /* Index of opposite cid in cids array */
+    uint64_t fromIndex;
+    /* Index of user stake in stakes array */
+    double weight;
+} CompressedInLink;
 
 #endif
