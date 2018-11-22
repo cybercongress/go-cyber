70dcb200fd55f591c58f0e53299a5fcc1cf58df7
Hleb Albau <hleb.albau@gmail.com>
2018-11-22 10:51:40 +0300

    #21 Use sum of lns as weight function

--- research/ethereum/common/adjacency_list_to_graph.py
+++ research/ethereum/common/adjacency_list_to_graph.py
@@ -6,6 +6,8 @@ from typing import Dict, Tuple
 import humanize
 import networkx as nx
 import math
+
+import numpy as np
 from networkx import DiGraph
 
 from common.tools import human_readable_interval
@@ -34,7 +36,7 @@ def load_edges(data_file: str) -> Edges:
         if cells[2] == "0":
             continue
         edge = (cells[0], cells[1])
-        weight = math.sqrt(float(cells[2]))
+        weight = np.log(float(cells[2]))
 
         if edge not in edges:
             edges[edge] = weight
