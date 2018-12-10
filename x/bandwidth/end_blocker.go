1702a8a245bf774afdb178d8af4c51eef1a41215
Artur Albov <artur.albov@gmail.com>
2018-12-10 20:04:35 +0300

    #77 Add bandwidth by stake. Bug fixes

--- x/bandwidth/end_blocker.go
+++ x/bandwidth/end_blocker.go
@@ -6,10 +6,17 @@ import (
 	"math"
 )
 
-func EndBlocker(ctx sdk.Context, totalSpentForPeriod uint64, ms storage.MainStorage) uint64 {
+func EndBlocker(
+	ctx sdk.Context, totalSpentForPeriod uint64, curPrice float64, ms storage.MainStorage,
+) (newPrice float64, totalSpent uint64) {
+	newPrice = curPrice
 	if ctx.BlockHeight() != 0 && ctx.BlockHeight()%AdjustPricePeriod == 0 {
 		averageSpentValue := MaxNetworkBandwidth / (RecoveryPeriod / AdjustPricePeriod)
-		newPrice := float64(totalSpentForPeriod) / float64(averageSpentValue)
+		newPrice = float64(totalSpentForPeriod) / float64(averageSpentValue)
+
+		if newPrice < 0.1*float64(BaseCreditPrice) {
+			newPrice = 0.1 * float64(BaseCreditPrice)
+		}
 
 		ms.StoreBandwidthPrice(ctx, math.Float64bits(newPrice))
 		ms.StoreSpentBandwidth(ctx, 0)
@@ -17,5 +24,5 @@ func EndBlocker(ctx sdk.Context, totalSpentForPeriod uint64, ms storage.MainStor
 	}
 
 	ms.StoreSpentBandwidth(ctx, totalSpentForPeriod)
-	return totalSpentForPeriod
+	return newPrice, totalSpentForPeriod
 }
