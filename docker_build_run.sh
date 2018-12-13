9b85bc4d592bbe8b39ad4e3e3c3601f4df4bbb82
Hleb Albau <hleb.albau@gmail.com>
2018-12-13 19:02:59 +0300

    Delete docker_build_run.sh

--- docker_build_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t cybercongress/cyberd .
-docker run -d --name=cyberd -p 26656:26656 -p 26657:26657 -p 26660:26660 -v ~/.cyberd/docker/data:/root/.cyberd/data -v ~/.cyberd/docker/config:/root/.cyberd/config cybercongress/cyberd:latest
