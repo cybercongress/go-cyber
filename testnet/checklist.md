88b512de66ebd2030c4817df52483a015f6a96b5
Hleb Albau <hleb.albau@gmail.com>
2018-12-13 10:41:37 +0300

    [euler-dev1] Fix initial loose tokens.

---
+++ testnet/checklist.md
@@ -0,0 +1,6 @@
+# Before run new testnet check:
+
+1. loose coins total amount  = initial supply
+2. adjust genesis supply in x/mint
+3. cyberd repo change chain-id
+4. reset old state
--- x/mint/params.go
+++ x/mint/params.go
@@ -5,7 +5,7 @@ const (
 	// assuming 1 second block times
 	BlocksPerYear = 60 * 60 * 24 * 365
 	// todo hardcoded
-	GenesisSupply = uint64(1000) * 1000 * 1000
+	GenesisSupply = uint64(17) * 1000 * 1000 * 1000
 	// percentage value 0....1, 1 means 100% year inflation
 	InflationRatePerYear = 1
 )
