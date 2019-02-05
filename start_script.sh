c53cde88c42de4e40a94eea7e5fddd15f2c405d7
Artur Albov <artur.albov@gmail.com>
2019-02-05 18:47:17 +0700

    #233 Rest server blocks cli keystore file

--- start_script.sh
+++ start_script.sh
@@ -16,7 +16,7 @@ if [ $status -ne 0 ]; then
 fi
 
 # Start the second process
-cyberdcli rest-server --cors=* --trust-node --chain-id=euler-1 --laddr=tcp://0.0.0.0:1317 --indent --insecure . &
+cyberdcli rest-server --cors=* --trust-node --chain-id=euler-1 --laddr=tcp://0.0.0.0:1317 --indent --insecure --home=/root/.cyberdcli_fakepath . &
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start cyberd light-client: $status"
