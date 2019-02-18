8ccdd4e887dd4c2a40cb84f4d57ad5153b40e9d0
Artur Albov <artur.albov@gmail.com>
2019-02-18 15:27:36 +0700

    #243 Consensus failure after node restart

--- x/rank/rank.go
+++ x/rank/rank.go
@@ -10,6 +10,7 @@ import (
 type Rank struct {
 	Values     []float64
 	MerkleTree *merkle.Tree
+	CidCount   uint64
 }
 
 func NewRank(values []float64, fullTree bool) Rank {
@@ -19,39 +20,49 @@ func NewRank(values []float64, fullTree bool) Rank {
 		binary.LittleEndian.PutUint64(rankBytes, math.Float64bits(f64))
 		merkleTree.Push(rankBytes)
 	}
-	return Rank{Values: values, MerkleTree: merkleTree}
+	return Rank{Values: values, MerkleTree: merkleTree, CidCount: uint64(len(values))}
 }
 
 func (r Rank) IsEmpty() bool {
-	return r.Values == nil || len(r.Values) == 0
+	return (r.Values == nil || len(r.Values) == 0) && r.MerkleTree == nil
 }
 
 func (r *Rank) Clear() {
 	r.Values = nil
 	r.MerkleTree = nil
+	r.CidCount = 0
 }
 
 func (r *Rank) CopyWithoutTree() Rank {
+
+	if r.Values == nil {
+		return Rank{Values: nil, MerkleTree: nil, CidCount: 0}
+	}
+
 	copiedValues := make([]float64, len(r.Values))
 	n := copy(copiedValues, r.Values)
 	// SHOULD NOT HAPPEN
 	if n != len(r.Values) {
 		panic("Not all rank values have been copied")
 	}
-	return Rank{Values: copiedValues, MerkleTree: nil}
+	return Rank{Values: copiedValues, MerkleTree: nil, CidCount: r.CidCount}
 }
 
 // TODO: optimize. Possible solution: adjust capacity of rank values slice after rank calculation
 func (r *Rank) AddNewCids(currentCidCount uint64) {
-	if r.Values == nil {
-		return
-	}
-	newCidsCount := currentCidCount - uint64(len(r.Values))
+
+	newCidsCount := currentCidCount - r.CidCount
+
 	// add new cids with rank = 0
-	r.Values = append(r.Values, make([]float64, newCidsCount)...)
+	if r.Values != nil {
+		r.Values = append(r.Values, make([]float64, newCidsCount)...)
+	}
+
 	// extend merkle tree
 	zeroRankBytes := make([]byte, 8)
 	for i := uint64(0); i < newCidsCount; i++ {
 		r.MerkleTree.Push(zeroRankBytes)
 	}
+
+	r.CidCount = currentCidCount
 }
