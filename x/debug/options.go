f3899547f881abcbf986f8d0b9c285f13b447d7d
Hleb Albau <hleb.albau@gmail.com>
2019-03-05 15:09:09 +0700

    #288 Move debug code to x/debug

---
+++ x/debug/options.go
@@ -0,0 +1,6 @@
+package debug
+
+type Options struct {
+	FailBeforeBlock           int64
+	FailRandomlyInNextNBlocks int64
+}
---
+++ x/debug/state.go
@@ -0,0 +1,6 @@
+package debug
+
+type State struct {
+	Opts         Options
+	StartupBlock int64
+}
