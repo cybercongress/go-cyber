d4b4438b0b605ce6f24568751ee2d3350a713189
Valery Litvin <litvintech@gmail.com>
2019-11-14 17:48:46 +0300

    Rollback of graph transformer processor

--- x/rank/internal/keeper/calculate_gpu.go
+++ x/rank/internal/keeper/calculate_gpu.go
@@ -3,12 +3,11 @@
 package keeper
 
 import (
-	"github.com/cybercongress/cyberd/x/link"
-	"github.com/cybercongress/cyberd/x/rank/internal/types"
 	"github.com/tendermint/tendermint/libs/log"
 
-	"math"
-	"sync"
+	"github.com/cybercongress/cyberd/x/link"
+	"github.com/cybercongress/cyberd/x/rank/internal/types"
+
 	"time"
 )
 
@@ -31,7 +30,7 @@ func calculateRankGPU(ctx *types.CalculationContext, logger log.Logger) []float6
 	outLinks := ctx.GetOutLinks()
 
 	cidsCount := ctx.GetCidsCount()
-	linksCount := uint64(0)
+	//linksCount := uint64(0)
 	stakesCount := len(ctx.GetStakes())
 
 	rank := make([]float64, cidsCount)
@@ -43,66 +42,34 @@ func calculateRankGPU(ctx *types.CalculationContext, logger log.Logger) []float6
 	outLinksUsers := make([]uint64, 0)
 
 	// todo reduce size of stake by passing only participating in linking stakes.
-	// todo need to investigate why GetStakes returns accounts with one missed index, think this is goes from some module
+	// todo need to investigate why GetStakes returns accounts with one missed index, think this is goes from some sdk module
 	stakes := make([]uint64, stakesCount+10)
 	for acc, stake := range ctx.GetStakes() {
 		stakes[uint64(acc)] = stake
 	}
 
-	ch := make(chan int64, 100000)
-	var wg sync.WaitGroup
-	var lock1 sync.Mutex
-	var lock2 sync.Mutex
-	wg.Add(int(cidsCount))
+	for i := int64(0); i < cidsCount; i++ {
 
-	// the worker's function
-	f := func(i int64) {
-		defer wg.Done()
 		if inLinks, sortedCids, ok := ctx.GetSortedInLinks(link.CidNumber(i)); ok {
 			for _, cid := range sortedCids {
-				inLinksCount[i] += uint32(len(inLinks[cid]))
+				inLinksCount[i]  = uint32(len(inLinks[cid]))
 				for acc := range inLinks[cid] {
-					lock2.Lock()
 					inLinksOuts = append(inLinksOuts, uint64(cid))
 					inLinksUsers = append(inLinksUsers, uint64(acc))
-					lock2.Unlock()
 				}
 			}
-			linksCount += uint64(inLinksCount[i])
+			//linksCount  = uint64(inLinksCount[i])
 		}
 
 		if outLinks, ok := outLinks[link.CidNumber(i)]; ok {
 			for _, accs := range outLinks {
-				outLinksCount[i] += uint32(len(accs))
+				outLinksCount[i]  = uint32(len(accs))
 				for acc := range accs {
-					lock1.Lock()
 					outLinksUsers = append(outLinksUsers, uint64(acc))
-					lock1.Unlock()
 				}
 			}
 		}
 	}
-
-	countWorkers := int64(math.Min(10000, float64(cidsCount)))
-
-	// here the workers start
-	for i:=int64(0); i < countWorkers; i++ {
-		go func() {
-			var cid int64
-			for {
-				cid = <- ch
-				f(cid)
-			}
-		}()
-	}
-
-	// data is added to the channel for workers
-	for i := int64(0); i < cidsCount; i++ {
-		ch <- i
-	}
-
-	// waiting for a while all workers will finish work
-	wg.Wait()
 
 	/* Convert to C types */
 	cStakes := (*C.ulong)(&stakes[0])
