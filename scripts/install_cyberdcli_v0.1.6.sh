613d6811cc84e6990425064d6bf4f5f5f4a75edd
Kiryl <laptanovi4@gmail.com>
2020-03-30 14:55:38 +0300

    add cosmwasm_lib to installation

--- scripts/install_cyberdcli_v0.1.6.sh
+++ scripts/install_cyberdcli_v0.1.6.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-# Installation script for cybercli. It tries to move binaries in one of the
+# Installation script for cyber. It tries to move $bin in one of the
 # directories stored in $binpaths.
 
-INSTALL_DIR=$(dirname $0)
-
 binpaths="/usr/local/bin /usr/bin"
+libpaths="/usr/lib /usr/local/lib"
 
 
 # This variable contains a nonzero length string in case the script fails
@@ -16,11 +15,25 @@ PLATFORM=$(uname)
 case "$PLATFORM" in
   "Darwin")
     # macOS
-    wget https://mars.cybernode.ai/go-cyber/go_cyber_v0.1.6_darwin-amd64.tar.gz
+    curl  https://mars.cybernode.ai/go-cyber/go_cyber_v0.1.6_darwin-amd64.tar.gz --output go_cyber_v0.1.6_darwin-amd64.tar.gz
+    tar -xzf go_cyber_v0.1.6_darwin-amd64.tar.gz -C ./
     for binpath in $binpaths; do
-      if tar -xzf go_cyber_v0.1.6_darwin-amd64.tar.gz -C "$binpath"; then
-        echo "Moved cybercli to $binpath"
+      if cp build_v0.1.6_darwin_amd64/cyberdcli "$binpath"; then
+        for libpath in $libpaths; do
+          if cp build_v0.1.6_darwin_amd64/libgo_cosmwasm.dylib  "$libpath"; then
+            cp build_v0.1.6_darwin_amd64/libgo_cosmwasm.so  "$libpath"
+            echo "Moved libgo_cosmwasm to $libpath"
+            break
+          else
+          if [ -d "$libpath" ] && [ ! -w "$libpath" ]; then
+            is_write_perm_missing=1
+          fi
+          fi
+        done
+        echo "Moved $bin to $binpath"
+        echo "Enjoy your cyber experience!"
         rm go_cyber_v0.1.6_darwin-amd64.tar.gz
+        rm -rf build_v0.1.6_darwin_amd64
         exit 0
       else
       if [ -d "$binpath" ] && [ ! -w "$binpath" ]; then
@@ -30,12 +43,25 @@ case "$PLATFORM" in
       fi
     done
     ;;
-  "Linux")
+      "Linux")
     # Linux distro,
-    wget https://mars.cybernode.ai/go-cyber/go_cyber_v0.1.6_linux-amd64.tar.gz
+    curl https://mars.cybernode.ai/go-cyber/go_cyber_v0.1.6_linux-amd64.tar.gz --output go_cyber_v0.1.6_linux-amd64.tar.gz
+    tar -xzf go_cyber_v0.1.6_linux-amd64.tar.gz -C ./
     for binpath in $binpaths; do
-      if tar -xzf go_cyber_v0.1.6_linux-amd64.tar.gz -C "$binpath"; then
-        echo "Moved cybercli to $binpath"
+      if cp build_v0.1.6_linux_amd64/cyberdcli "$binpath"; then
+        for libpath in $libpaths; do
+          if cp build_v0.1.6_linux_amd64/libgo_cosmwasm.dylib  "$libpath"; then
+            cp build_v0.1.6_linux_amd64/libgo_cosmwasm.so  "$libpath"
+            echo "Moved libgo_cosmwasm to $libpath"
+            break
+          else
+          if [ -d "$libpath" ] && [ ! -w "$libpath" ]; then
+            is_write_perm_missing=1
+          fi
+          fi
+        done
+        echo "Moved $bin to $binpath"
+        echo "Enjoy your cyber experience!"
         rm go_cyber_v0.1.6_linux-amd64.tar.gz
         exit 0
       else
@@ -49,12 +75,12 @@ case "$PLATFORM" in
 esac
 
 
-echo "We cannot install cybercli in one of the directories $binpaths"
+echo "We cannot install $bin in one of the directories $binpaths"
 
 if [ -n "$is_write_perm_missing" ]; then
   echo "It seems that we do not have the necessary write permissions."
   echo "Perhaps try running this script as a privileged user:"
-  echo
+  echo "Or check that you using default library path."
   echo "    sudo $0"
   echo
 fi
