3268ef98c1c18a9094300e2589870b9edb11e693
Valery Litvin <litvintech@gmail.com>
2020-07-17 14:19:14 +0300

    Changed upgrade name to darwin

--- app/app.go
+++ app/app.go
@@ -200,7 +200,7 @@ func NewCyberdApp(logger log.Logger, db dbm.DB, traceStore io.Writer, loadLatest
 	)
 	app.upgradeKeeper = upgrade.NewKeeper(skipUpgradeHeights, dbKeys.upgrade, app.cdc)
 
-	app.upgradeKeeper.SetUpgradeHandler("cycle-0", func(ctx sdk.Context, plan upgrade.Plan) {})
+	app.upgradeKeeper.SetUpgradeHandler("darwin", func(ctx sdk.Context, plan upgrade.Plan) {})
 
 	var wasmRouter = baseApp.Router()
 	homeDir := viper.GetString(cli.HomeFlag)
