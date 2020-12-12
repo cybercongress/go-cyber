5708abceea9bcc0b701d9b6a625a05af032129a2
Valery Litvin <litvintech@gmail.com>
2020-12-12 18:01:13 +0800

    Fixed call of rank rpc endpoint

--- cmd/cyberd/rpc/rank.go
+++ cmd/cyberd/rpc/rank.go
@@ -1,15 +1,17 @@
 package rpc
 
 import (
+	rpctypes "github.com/tendermint/tendermint/rpc/jsonrpc/types"
+
 	"github.com/cybercongress/go-cyber/merkle"
 )
 
 type RankAndProofResult struct {
 	Proofs []merkle.Proof `json:"proofs"`
-	Rank   float64        `amino:"unsafe" json:"rank"`
+	Rank   uint64          `json:"rank"`
 }
 
-func Rank(cid string, proof bool) (*RankAndProofResult, error) {
+func Rank(ctx *rpctypes.Context, cid string, proof bool) (*RankAndProofResult, error) {
 	rankValue, proofs, err := cyberdApp.Rank(cid, proof)
 	return &RankAndProofResult{proofs, rankValue}, err
 }
