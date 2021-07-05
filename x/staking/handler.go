1dcfb23d4b68f525b5e1a13469fb5d3deb98e0b1
Valery Litvin <litvintech@gmail.com>
2021-07-05 15:56:38 +0800

    Added minting of proto on validator creation

--- x/staking/handler.go
+++ x/staking/handler.go
@@ -35,14 +35,14 @@ func WrapStakingHandler(
 			if result == false {
 				return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "unauthorized message: %T", msg)
 			}
-		//case *stakingtypes.MsgCreateValidator:
-		//	result, err := ValidateCreateValidator(msg)
-		//	if err != nil {
-		//		return nil, err
-		//	}
-		//	if result == false {
-		//		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "unauthorized message: %T", msg)
-		//	}
+		case *stakingtypes.MsgCreateValidator:
+			result, err := ValidateCreateValidator(ctx, bk , msg)
+			if err != nil {
+				return nil, err
+			}
+			if result == false {
+				return nil, sdkerrors.Wrapf(sdkerrors.ErrUnauthorized, "unauthorized message: %T", msg)
+			}
 		}
 		handler := staking.NewHandler(sk)
 		return handler(ctx, msg)
@@ -89,10 +89,26 @@ func ProcessUndelegate(ctx sdk.Context,
 	return true, nil
 }
 
-func ValidateCreateValidator(msg *stakingtypes.MsgCreateValidator) (bool, error) {
-	if msg.MinSelfDelegation.GTE(sdk.NewInt(10*ctypes.Giga)) {
-		return true, nil
+func ValidateCreateValidator(ctx sdk.Context,
+	bk bankkeeper.Keeper, msg *stakingtypes.MsgCreateValidator,
+) (bool, error) {
+	//if msg.MinSelfDelegation.GTE(sdk.NewInt(10*ctypes.Giga)) {
+	//	return true, nil
+	//}
+	delegator, err := sdk.AccAddressFromBech32(msg.DelegatorAddress); if err != nil {
+		return false, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "bad account")
 	}
+
+	toMint := sdk.NewCoin(ctypes.SCYB, msg.Value.Amount)
+	err = bk.MintCoins(ctx, types.ResourcesName, sdk.NewCoins(toMint))
+	if err != nil {
+		return false, sdkerrors.Wrapf(types.ErrMintCoins, delegator.String())
+	}
+	err = bk.SendCoinsFromModuleToAccount(ctx, types.ResourcesName, delegator, sdk.NewCoins(toMint))
+	if err != nil {
+		return false, sdkerrors.Wrapf(types.ErrSendMintedCoins, delegator.String())
+	}
+
 
 	return false, nil
 }
