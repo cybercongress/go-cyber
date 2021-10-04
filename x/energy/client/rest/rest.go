64978d51e00f653f8f8005e9373f69c15f7c8eb3
Valery Litvin <litvintech@gmail.com>
2021-10-04 14:20:20 +0800

    Revoved rest txs from energy and graph modules

--- x/energy/client/rest/rest.go
+++ x/energy/client/rest/rest.go
@@ -2,7 +2,6 @@ package rest
 
 import (
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/types/rest"
 	"github.com/gorilla/mux"
 )
 
@@ -14,28 +13,4 @@ const (
 // RegisterRoutes registers power-related REST handlers to a router
 func RegisterRoutes(cliCtx client.Context, r *mux.Router) {
 	registerQueryRoutes(cliCtx, r)
-	registerTxRoutes(cliCtx, r)
-}
-
-type CreateRouteReq struct {
-	BaseReq rest.BaseReq 		`json:"base_req" yaml:"base_req"`
-	Destination  string 		`json:"destination" yaml:"destination"`
-	Alias 		 string 		`json:"alias" yaml:"alias"`
-}
-
-type EditRouteReq struct {
-	BaseReq rest.BaseReq 		`json:"base_req" yaml:"base_req"`
-	Destination  string 		`json:"destination" yaml:"destination"`
-	Value 		 string 		`json:"value" yaml:"value"`
-}
-
-type DeleteRouteReq struct {
-	BaseReq rest.BaseReq 		`json:"base_req" yaml:"base_req"`
-	Destination  string 		`json:"destination" yaml:"destination"`
-}
-
-type EditRouteAliasReq struct {
-	BaseReq rest.BaseReq 		`json:"base_req" yaml:"base_req"`
-	Destination  string		    `json:"destination" yaml:"destination"`
-	Alias 		 string 		`json:"alias" yaml:"alias"`
 }
--- x/graph/client/rest/rest.go
+++ x/graph/client/rest/rest.go
@@ -3,18 +3,9 @@ package rest
 import (
 	"github.com/cosmos/cosmos-sdk/client"
 
-	"github.com/cosmos/cosmos-sdk/types/rest"
 	"github.com/gorilla/mux"
-
-	"github.com/cybercongress/go-cyber/x/graph/types"
 )
 
 func RegisterRoutes(cliCtx client.Context, r *mux.Router) {
 	registerQueryRoutes(cliCtx, r)
-	registerTxRoutes(cliCtx, r)
-}
-
-type CreateCyberlinkReq struct {
-	BaseReq        rest.BaseReq       `json:"base_req"`
-	Links		   []types.Link 	  `json:"links"`
 }
