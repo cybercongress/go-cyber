ff86b02cee228baee1459e347900671d5a214e43
C H <litvintech@gmail.com>
2024-02-22 18:32:44 +0800

    Refactoring of app structure

---
+++ app/upgrades/v2/upgrades.go
@@ -0,0 +1,19 @@
+package v2
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
+	"github.com/cybercongress/go-cyber/v2/app/keepers"
+)
+
+func CreateV2UpgradeHandler(
+	_ *module.Manager,
+	_ module.Configurator,
+	_ *keepers.AppKeepers,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		return vm, nil
+	}
+}
