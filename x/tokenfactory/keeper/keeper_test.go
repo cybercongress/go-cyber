501a733ef6629b46cf8c2d0f61e736aa63c2b7d0
C H <litvintech@gmail.com>
2024-04-27 19:51:34 +0800

    Enabled all module tests

--- x/tokenfactory/keeper/keeper_test.go
+++ x/tokenfactory/keeper/keeper_test.go
@@ -1,54 +1,63 @@
 package keeper_test
 
-//type KeeperTestSuite struct {
-//	apptesting.KeeperTestHelper
-//
-//	queryClient     types.QueryClient
-//	bankQueryClient banktypes.QueryClient
-//	msgServer       types.MsgServer
-//	// defaultDenom is on the suite, as it depends on the creator test address.
-//	defaultDenom string
-//}
-//
-//func TestKeeperTestSuite(t *testing.T) {
-//	suite.Run(t, new(KeeperTestSuite))
-//}
-//
-//func (suite *KeeperTestSuite) SetupTest() {
-//	suite.Setup()
-//
-//	// Fund every TestAcc with two denoms, one of which is the denom creation fee
-//	fundAccsAmount := sdk.NewCoins(sdk.NewCoin(types.DefaultParams().DenomCreationFee[0].Denom, types.DefaultParams().DenomCreationFee[0].Amount.MulRaw(100)), sdk.NewCoin(apptesting.SecondaryDenom, apptesting.SecondaryAmount))
-//	for _, acc := range suite.TestAccs {
-//		suite.FundAcc(acc, fundAccsAmount)
-//	}
-//
-//	suite.queryClient = types.NewQueryClient(suite.QueryHelper)
-//	suite.bankQueryClient = banktypes.NewQueryClient(suite.QueryHelper)
-//	suite.msgServer = keeper.NewMsgServerImpl(suite.App.AppKeepers.TokenFactoryKeeper)
-//}
-//
-//func (suite *KeeperTestSuite) CreateDefaultDenom() {
-//	res, _ := suite.msgServer.CreateDenom(sdk.WrapSDKContext(suite.Ctx), types.NewMsgCreateDenom(suite.TestAccs[0].String(), "bitcoin"))
-//	suite.defaultDenom = res.GetNewTokenDenom()
-//}
-//
-//func (suite *KeeperTestSuite) TestCreateModuleAccount() {
-//	app := suite.App
-//
-//	// remove module account
-//	tokenfactoryModuleAccount := app.AppKeepers.AccountKeeper.GetAccount(suite.Ctx, app.AppKeepers.AccountKeeper.GetModuleAddress(types.ModuleName))
-//	app.AppKeepers.AccountKeeper.RemoveAccount(suite.Ctx, tokenfactoryModuleAccount)
-//
-//	// ensure module account was removed
-//	suite.Ctx = app.BaseApp.NewContext(false, tmproto.Header{ChainID: "testing"})
-//	tokenfactoryModuleAccount = app.AppKeepers.AccountKeeper.GetAccount(suite.Ctx, app.AppKeepers.AccountKeeper.GetModuleAddress(types.ModuleName))
-//	suite.Require().Nil(tokenfactoryModuleAccount)
-//
-//	// create module account
-//	app.AppKeepers.TokenFactoryKeeper.CreateModuleAccount(suite.Ctx)
-//
-//	// check that the module account is now initialized
-//	tokenfactoryModuleAccount = app.AppKeepers.AccountKeeper.GetAccount(suite.Ctx, app.AppKeepers.AccountKeeper.GetModuleAddress(types.ModuleName))
-//	suite.Require().NotNil(tokenfactoryModuleAccount)
-//}
+import (
+	tmproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/cybercongress/go-cyber/v4/app/apptesting"
+	"github.com/cybercongress/go-cyber/v4/x/tokenfactory/types"
+	"github.com/stretchr/testify/suite"
+)
+
+type KeeperTestSuite struct {
+	apptesting.KeeperTestHelper
+
+	queryClient     types.QueryClient
+	bankQueryClient banktypes.QueryClient
+	msgServer       types.MsgServer
+	// defaultDenom is on the suite, as it depends on the creator test address.
+	defaultDenom string
+}
+
+func TestKeeperTestSuite(t *testing.T) {
+	suite.Run(t, new(KeeperTestSuite))
+}
+
+func (suite *KeeperTestSuite) SetupTest() {
+	suite.Setup()
+
+	// Fund every TestAcc with two denoms, one of which is the denom creation fee
+	fundAccsAmount := sdk.NewCoins(sdk.NewCoin(types.DefaultParams().DenomCreationFee[0].Denom, types.DefaultParams().DenomCreationFee[0].Amount.MulRaw(100)), sdk.NewCoin(apptesting.SecondaryDenom, apptesting.SecondaryAmount))
+	for _, acc := range suite.TestAccs {
+		suite.FundAcc(acc, fundAccsAmount)
+	}
+
+	suite.queryClient = types.NewQueryClient(suite.QueryHelper)
+	suite.bankQueryClient = banktypes.NewQueryClient(suite.QueryHelper)
+	suite.msgServer = keeper.NewMsgServerImpl(suite.App.AppKeepers.TokenFactoryKeeper)
+}
+
+func (suite *KeeperTestSuite) CreateDefaultDenom() {
+	res, _ := suite.msgServer.CreateDenom(sdk.WrapSDKContext(suite.Ctx), types.NewMsgCreateDenom(suite.TestAccs[0].String(), "bitcoin"))
+	suite.defaultDenom = res.GetNewTokenDenom()
+}
+
+func (suite *KeeperTestSuite) TestCreateModuleAccount() {
+	app := suite.App
+
+	// remove module account
+	tokenfactoryModuleAccount := app.AppKeepers.AccountKeeper.GetAccount(suite.Ctx, app.AppKeepers.AccountKeeper.GetModuleAddress(types.ModuleName))
+	app.AppKeepers.AccountKeeper.RemoveAccount(suite.Ctx, tokenfactoryModuleAccount)
+
+	// ensure module account was removed
+	suite.Ctx = app.BaseApp.NewContext(false, tmproto.Header{ChainID: "testing"})
+	tokenfactoryModuleAccount = app.AppKeepers.AccountKeeper.GetAccount(suite.Ctx, app.AppKeepers.AccountKeeper.GetModuleAddress(types.ModuleName))
+	suite.Require().Nil(tokenfactoryModuleAccount)
+
+	// create module account
+	app.AppKeepers.TokenFactoryKeeper.CreateModuleAccount(suite.Ctx)
+
+	// check that the module account is now initialized
+	tokenfactoryModuleAccount = app.AppKeepers.AccountKeeper.GetAccount(suite.Ctx, app.AppKeepers.AccountKeeper.GetModuleAddress(types.ModuleName))
+	suite.Require().NotNil(tokenfactoryModuleAccount)
+}
--- x/tokenfactory/module.go
+++ x/tokenfactory/module.go
@@ -24,12 +24,9 @@ import (
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
-	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-
 	"github.com/cybercongress/go-cyber/v4/x/tokenfactory/client/cli"
 	"github.com/cybercongress/go-cyber/v4/x/tokenfactory/exported"
 	"github.com/cybercongress/go-cyber/v4/x/tokenfactory/keeper"
-	simulation "github.com/cybercongress/go-cyber/v4/x/tokenfactory/simulation"
 	"github.com/cybercongress/go-cyber/v4/x/tokenfactory/types"
 )
 
@@ -185,47 +182,4 @@ func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
 // returns no validator updates.
 func (am AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
 	return []abci.ValidatorUpdate{}
-}
-
-// ___________________________________________________________________________
-
-// AppModuleSimulationV2 functions
-
-// // GenerateGenesisState creates a randomized GenState of the tokenfactory module.
-// func (am AppModule) SimulatorGenesisState(simState *module.SimulationState, s *simtypes.SimCtx) {
-// 	tfDefaultGen := types.DefaultGenesis()
-// 	tfDefaultGen.Params.DenomCreationFee = sdk.NewCoins(sdk.NewCoin(appparams.BondDenom, sdk.NewInt(10000000)))
-// 	tfDefaultGenJson := simState.Cdc.MustMarshalJSON(tfDefaultGen)
-// 	simState.GenState[types.ModuleName] = tfDefaultGenJson
-// }
-
-// // WeightedOperations returns the all the lockup module operations with their respective weights.
-// func (am AppModule) Actions() []simtypes.Action {
-// 	return []simtypes.Action{
-// 		simtypes.NewMsgBasedAction("create token factory token", am.keeper, simulation.RandomMsgCreateDenom),
-// 		simtypes.NewMsgBasedAction("mint token factory token", am.keeper, simulation.RandomMsgMintDenom),
-// 		simtypes.NewMsgBasedAction("burn token factory token", am.keeper, simulation.RandomMsgBurnDenom),
-// 		simtypes.NewMsgBasedAction("change admin token factory token", am.keeper, simulation.RandomMsgChangeAdmin),
-// 	}
-// }
-
-// ____________________________________________________________________________
-
-// AppModuleSimulation functions
-func (AppModule) GenerateGenesisState(simState *module.SimulationState) {
-	simulation.RandomizedGenState(simState)
-}
-
-// GenerateGenesisState creates a randomized GenState of the bank module.
-func (am AppModule) ProposalContents(_ module.SimulationState) []simtypes.WeightedProposalMsg {
-	return nil
-}
-
-// RegisterStoreDecoder registers a decoder for supply module's types
-func (am AppModule) RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {
-}
-
-// WeightedOperations returns the all the gov module operations with their respective weights.
-func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
-	return simulation.WeightedOperations(&simState, am.keeper, am.accountKeeper, am.bankKeeper)
 }
